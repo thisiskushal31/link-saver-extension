b2cff9119f553873ff8192da8de8b9f09c016596
thisiskushal31 <guptakushal131@gmail.com>
2022-03-10 21:37:21 +0530

    More Feature Added

---
+++ app/js/script.js
@@ -0,0 +1,25 @@
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads"))
+const tabBtn = document.getElementById("tab-btn")
+
+tabBtn.addEventListener("click", function(){    
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    })
+})
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+})
+
+function create()
+{
+    window.location = 'savedata.html';
+}
